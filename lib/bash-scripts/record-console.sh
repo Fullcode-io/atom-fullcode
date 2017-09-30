9cee960c7cd5de7100fd7de046a41ed063ade2ba
Luiz Neves <luizneves@Luizs-MacBook-Pro.local>
2017-09-30 00:47:24 -0500

    update record-console.sh

--- lib/bash-scripts/record-console.sh
+++ lib/bash-scripts/record-console.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 LINE_TO_ADD="source ~/.nighthawk/record-console.sh"
-# can't put in bashrc sometimes.... linux/permissions
 BASHRC_PATH=$HOME/.bashrc
 BASH_PROFILE_PATH=$HOME/.bash_profile
 
@@ -21,7 +20,6 @@ move_line_to_bottom()
 }
 
 install_script() {
-  echo "$1"
   check_if_last_line $1 || move_line_to_bottom $1
 }
 
@@ -32,67 +30,49 @@ ask_to_start_tracking()
   if [[ $REPLY =~ ^[Yy]$ ]]
   then
     addMetadata() {
-      while IFS= read -r line; do
-        if [[ $line != *$' exit\r' ]]
+      if [[ -z $SCRIPT ]]
         then
-          # linux-gne also seems to do an extra script layer... not sure why
-          PARENTPID=$(pgrep -P "$$" "script" | awk '{system("echo " $1)}')
-          # # linux has an extra process layer for some reason
-          pgrep -P "$PARENTPID" "script" && PARENTPID=$(pgrep -P "$PARENTPID" "script" | awk '{system("echo " $1)}')
+          echo 'Nighthawk running'
+      fi
 
+      while IFS= read -r line; do
+          PARENTPID=$(pgrep -P "$$" "script" | awk '{system("echo " $1)}')
+          if [[ -z $PARENTPID ]]; then return 0; fi
+          # # linux ssh shells have an extra process layer
+          # pgrep -P "$PARENTPID" "script" && PARENTPID=$(pgrep -P "$PARENTPID" "script" | awk '{system("echo " $1)}')
           CHILDPID=$(pgrep -P "$PARENTPID" "bash" | awk '{system("echo " $1)}')
+          if [[ -z $CHILDPID ]]; then return 0; fi
           # # TODO: pwdx solution for linux
           CWD=$(lsof -p "$CHILDPID" | grep "cwd" | awk '{system("echo " $9)}')
-          # printf "%s\n" "current-cwd: $CWD/\r"
-          # x=0
-          printf "%s" "$CWD/ |//🐦//| $(date +%s) |//🐦//| $line"
-        else
-          return
-        fi
+          printf "%s\n" "$CWD/ |//🐦//| $(date +%s) |//🐦//| $line"
 
-        printf '\n'
-        # printf "%s %s\n" "$(date)" "$line" | tr -cd '[:print:]\r\t\v\x2705'
-        # printf "%s %s\n" "$(date +%s):" "$line" | tr -cd '[:print:]\r\t'
-
-
-        # printf "%s %s\n" "$(date)" "$line" | sed -e 's/[^A-Za-z0-9._-!?@#$%^&*()=+|{}<>,|`~"]/??/g'
       done >> $HOME/.nighthawk/logs/session.log
-      # done >> $HOME/tmp/test.txt
-        # echo "$line"
-      # done
     }
-    printf "%s\n" "Logging Terminal Session..."
-    # save prompt here to reference later in sub-process via bashrc
-    export ORIGPS1=$PS1
     # linux requires lowercase -f here
     [[ $OSTYPE == *"linux"* ]] && f=f || f=F
-    script -a -q -$f >(addMetadata)
-    # script -a -q -$f 2> >(tee /dev/null >&2 >(addMetadata))
-    # exec 2> >(tee /dev/null >&2 >(addMetadata))
-    # exec 2> >(addMetadata >&2)
-    # exec 2> >(grep "" >&2)
-    # exec 2>&1 | tee -a $HOME/tmp/test.txt
-    # PS1=$ORIGPS1
+    env PS1="$PS1" script -a -q -$f >(addMetadata)
   fi
 }
 # if script is not last line then move it to bottom and return
-# check_if_last_line || move_line_to_bottom
 install_script $BASHRC_PATH
-install_script $BASH_PROFILE_PATH
 
 if [[ $1 = "no-track" ]]
   then
     echo 'exiting'
-elif [[ $(ps -fp "$PPID" | grep "script -a -q -" || ps -fp "$PPID" | grep "bash -i") ]]
-  then
-    # if we get here then we are in the new typescript session
-    # remove source to this script then reload bash_profile to set everything back to "normal"
-    # ex +g/record-console.sh/d -cwq "$BASH_PROFILE_PATH"
-    # source ~/.bash_profile
-    # move_line_to_bottom $BASH_PROFILE_PATH
-    echo "setting $PS1 to $ORIGPS1"
-    PS1=🐦\ $ORIGPS1
-    clear
+elif [[ ! -z $SCRIPT || $(ps -fp "$PPID" | grep "script -a -q -" || ps -fp "$PPID" | grep "bash -i") ]]
+    then
+      # if we get here then we are in the new typescript session
+      # remove source to this script then reload bash_profile to set everything back to "normal"
+      if [ "$NIGHTHAWK" != true ]
+        then
+          export NIGHTHAWK=true
+          echo 'reloading profile'
+          [ -s "$BASH_PROFILE_PATH" ] && source "$BASH_PROFILE_PATH"
+          PS1="🐦 $PS1"
+      else
+        echo 'already in nh script! not loading profile'
+      fi
+      return
   else
     ask_to_start_tracking
 fi
